fbdfcad7a80a61f8cc62335c8ac0b0d8b9a20a2b
lyonsn1 <lyonsn1@nationwide.com>
2018-01-10 10:09:37 -0500

    fixed page structure and css

--- _site/js/components/tabs.js
+++ _site/js/components/tabs.js
@@ -3,7 +3,6 @@ var tab_links = tab_container.querySelectorAll('.tab-links li');
 var tabs = tab_container.querySelectorAll('.tab-content');
 
 tab_links.forEach(function(link, index) {
-  console.log(link + " " + index);
   link.addEventListener('click', function(event) {
     removeActiveTab();
     tabs[index].classList.add('active-tab');
