1896b084d77ddd7fe7a856cb37f6a824b4b63994
lyonsn1 <lyonsn1@nationwide.com>
2018-01-23 12:25:28 -0500

    cleaned up for a final push, need to update css

--- _site/js/scripts/tipuesearch_content.js
+++ _site/js/scripts/tipuesearch_content.js
@@ -1,121 +1,111 @@
 var tipuesearch = {"pages": [{
-    "title": "Access &amp; Permission",
-    "text": "Access & Permissions Soap UI SQL Explorer",
-    "tags": "",
-    "url": "/onboarding/access-permissions/"
-  },{
     "title": "Cardwall",
     "text": "Cardwall Soap UI SQL Explorer",
     "tags": "",
-    "url": "/onboarding/cardwall/"
+    "url": "/lyonsn1/git-wiki/onboarding/cardwall/"
   },{
     "title": "Contacts and Teams",
     "text": "Contacts Click on the Name to open up in Outlook. Name Title Jamison Roberts Infix Tech Lead Janet Brenot Requirements Doug Baylor Requirements Lead Tara Lapper Release Tester Lead Trine Rowell Release Tester Jenny Newton Release Tester Storm Stidam Release Tester Bridgette Soisson Actuary Marvin Pennyman Actuary Anne Sheffield Actuary Victor Gordon Presentations Lead Parag Patil CM Dev Kiran Pendota CM Dev Marcy Stephenson BAO Jennifer A. Moore BAO Sarah Meyer Commissions Tester Joe Saunders Commissions Tester",
     "tags": "",
-    "url": "/contacts-teams/"
+    "url": "/lyonsn1/git-wiki/contacts-teams/"
   },{
     "title": "Defects",
     "text": "Defects Defects info goes here.",
     "tags": "",
-    "url": "/testing/defects/"
+    "url": "/lyonsn1/git-wiki/testing/defects/"
   },{
     "title": "Development",
-    "text": "Developer links and info goes here",
+    "text": "Developer Put information about development here!",
     "tags": "",
-    "url": "/development/home/"
+    "url": "/lyonsn1/git-wiki/development/home/"
   },{
     "title": "Testing",
     "text": "Financial Terms Basic Policy Terms Deductible The amount the customer must pay before insurance kicks in. Coverage A general term for all the types of coverages against possible risks. The more coverage the higher the premium. A general term for all the types of coverages against possible risks. The more coverage the higher the premium. Primary Insured Usually the policy holder, it refers to who the insurance will protect. MVR Motor Vehicle Records contain traffic violations and related information about the vehicle or driver. Secondary Insured Additional individual to be insured under the same coverages as the Primary.",
     "tags": "",
-    "url": "/glossary/finance/"
+    "url": "/lyonsn1/git-wiki/glossary/finance/"
   },{
     "title": "Testing",
     "text": "Glossary of Terms Financial Terms Business Terms Technical Terms",
     "tags": "",
-    "url": "/glossary/home/"
-  },{
-    "title": "How To’s",
-    "text": "How To's",
-    "tags": "",
-    "url": "/onboarding/how-tos/"
+    "url": "/lyonsn1/git-wiki/glossary/home/"
   },{
     "title": "icons",
     "text": "Icons",
     "tags": "",
-    "url": "/icons/"
+    "url": "/lyonsn1/git-wiki/icons/"
   },{
     "title": "home",
     "text": "Welcome to Line/Project Property and Auto insurance development line intro stuff goes here. Onboarding Projects Products Transactions Glossary of Terms Contacts and Teams Requirements Development Testing Wiki Guide",
     "tags": "",
-    "url": "/"
+    "url": "/lyonsn1/git-wiki/"
   },{
     "title": "onboarding",
-    "text": "Onboarding Here are some important links, checklists, and how-to's for getting started. Tools Access & Permissions Starter Docs Useful Links/Documents Project Overview Testing Overview Cardwall How To's Contacts & Teams",
+    "text": "Onboarding Here are some important links, checklists, and how-to's for getting started. Tools and Access Starter Docs Useful Links/Documents Project Overview Testing Overview Cardwall Contacts & Teams",
     "tags": "",
-    "url": "/onboarding/"
+    "url": "/lyonsn1/git-wiki/onboarding/"
   },{
     "title": "Overview",
     "text": "Line Testing Overview Testing Overview Info goes here.",
     "tags": "",
-    "url": "/testing/overview/"
+    "url": "/lyonsn1/git-wiki/testing/overview/"
   },{
     "title": "Products",
     "text": "Products Name Label Code IVUL Accumulator VUL Accumulator 31 IVUL Protector VUL Protector 32 Indexed Universal Life INDEXED UL 22 Indexed Universal Life Accumulator IUL Accumulator 28 Indexed Universal Life Protector IUL Protector 30 Accumulation Variable Universal Life Accum VUL 46 Survivorship Universal Life SUL 42 Protection Variable Universal Life PVUL 43 Single Premium Universal Life SPUL 45 Current Assumption Universal Life Current Assumption UL (CUAL) 44 No-Lapse Guarantee SUL II NLG SUL II 20 No-Lapse Guarantee UL II NLGULII 12 NGL NGL 18 NLGUL NLGUL 16 Long Term Care LTC 27 NBSG CVUL 1 NBSG Sa Boli 14 NBSG FEVUL 1 NBSG SVUL 4 NBSG CVUL 5 NBSG FEUL 13 NBSG BOLI 6 NBSG CVUL 1",
     "tags": "",
-    "url": "/products/home/"
+    "url": "/lyonsn1/git-wiki/products/home/"
   },{
     "title": "Project Overview",
     "text": "Project Overview Soap UI SQL Explorer",
     "tags": "",
-    "url": "/onboarding/project-overview/"
+    "url": "/lyonsn1/git-wiki/onboarding/project-overview/"
   },{
     "title": "PLT Projects",
     "text": "Comp Rater Copy this starter-page.html when creating a new page. Remove or alter components that fit your needs. Don't forget to update your links. Internet Copy this starter-page.html when creating a new page. Remove or alter components that fit your needs. Don't forget to update your links. Policy Center Copy this starter-page.html when creating a new page. Remove or alter components that fit your needs. Don't forget to update your links.",
     "tags": "",
-    "url": "/projects/home/"
+    "url": "/lyonsn1/git-wiki/projects/home/"
   },{
     "title": "Requirements",
-    "text": "Requirements links and info goes here",
+    "text": "Requirements Put information about requirements here!",
     "tags": "",
-    "url": "/requirements/home/"
+    "url": "/lyonsn1/git-wiki/requirements/home/"
   },{
     "title": "Starter Docs",
     "text": "Starter Docs",
     "tags": "",
-    "url": "/onboarding/starter-docs/"
+    "url": "/lyonsn1/git-wiki/onboarding/starter-docs/"
   },{
     "title": "home",
-    "text": "Style Guide Overview of the UI Components. Contact support @Nick Lyons. Text Sections Tables Lists Links Buttons Definition Lists Tabs Icon Box Text Sections Vertical Rhythm, or the spacing and proximity of headers and sub-headers in relation to paragraphs and lists is useful for visually communicating what is related. Notice the space above and below the headers, there is more above and less below to visually convey associated content. An equal distance above and below provides for a smooth read but doesn't show header to content relationship very well. Headers closer to their content shows a closeness, a relationship. It's a subtle effect. White space between headers shows distance or a distinct grouping of information. Use h2 headers for top level sections and use h3 for sub-sections or mini-chapters for a topic. These prinicples are coded for all elements on the page to provide a smoother reading experience. Hopefully... Vertical Rhythm Vertical Rhythm Vertical Rhythm Project Decomp What UI changes are happening, feed changes, database table validation. Ask the dev what will I be validating? The UI, the database tables, or a report somewhere? Testers and Dev's give a point estimate for suggested cards. Most story cards never exceed 5 points. If they do, the card is too big. You should find scenarios to move into another card. Don't estimate an 8 point card, decompose it into smaller cards. 3 Amigos RA creates story cards according to the SRD Business Rules. RA get's approval from BAO for the story card. Look at the RA Board for story card status. Bring laptop and ask the RA or Dev where in the UI or database tables the test can be validated. Write your notes in the RTC card, these will be helpful for the tester who picks up the card later. Tables Tables come into to types: Static or Data. Data tables allow you to use a YAML or CSV to populate the table from a file. Much easier for re-use and maintenance. Name Title Jamison Roberts Infix Tech Lead Janet Brenot Requirements Doug Baylor Requirements Lead Tara Lapper Release Tester Lead Trine Rowell Release Tester Jenny Newton Release Tester Storm Stidam Release Tester Bridgette Soisson Actuary Marvin Pennyman Actuary Anne Sheffield Actuary Victor Gordon Presentations Lead Parag Patil CM Dev Kiran Pendota CM Dev Marcy Stephenson BAO Jennifer A. Moore BAO Sarah Meyer Commissions Tester Joe Saunders Commissions Tester Lists List of Links Tools Access & Permissions Starter Docs Useful Links/Documents Project Overview Testing Overview Cardwall How To's Contacts & Teams Inline Link in a Paragrah What UI changes are happening, feed changes, database table validation. Ask the dev what will I be validating? The UI, the database tables, or a report somewhere? Testers and Dev's give a point estimate for suggested cards. Quick Links Quick Links list for article sections. Put an id=\"#bookmark\" on an element and reference it from the quick links. Quick links go at the beginning and basically short cuts to the article sections. About More Info Product Model State Variations Details of Product Types Product Codes Serialization Buttons Used for navigation to other pages. Colors are for variety! Home Onboarding Projects Back to Home Data Definition Lists Hide and Show definitions are perfect for keeping a growing list of business and technical terms. Basic Policy Terms Deductible The amount the customer must pay before insurance kicks in. Coverage A general term for all the types of coverages against possible risks. The more coverage the higher the premium. A general term for all the types of coverages against possible risks. The more coverage the higher the premium. Primary Insured Usually the policy holder, it refers to who the insurance will protect. MVR Motor Vehicle Records contain traffic violations and related information about the vehicle or driver. Secondary Insured Additional individual to be insured under the same coverages as the Primary. Tabs Tabs are great for displaying data with images and text. Story Card Tech Card Defect Card Error Card They are linked to RTC. Developer works on the card, then moves ready to test for line tester to work on. They are sent the BAO for Sign-Off when testing is complete. Tech cards are worked by only the developer. They are not tested by the line. Defect cards are worked on by Line Developer, then sent back to tester who created it. In Vegas line, these are defects found outside of the team and assigned to our dev's to fix. Line Error cards are fixed by Line Dev, then retested by Line Tester who found them. In Vegas line, they are 'defects' found within line testing. Icon Box Good for hightlighting a tip, trick, gotcha or a don't do. Warning: Don't Do this! This will cause permanent damage to the system. Or will screw up the database/policy/data sheet. This will cause permanent damage to the system. Or will screw up the database/policy/data sheet. Save for later. You'll use this information often for user testing. Keep it handy or bookmarked for later review. Save for later. You'll use this information often for user testing. Keep it handy or bookmarked for later review. Good Idea! Try this to save time, or a nifty process to assist in what ever you are doing. System/Computer Configuration! Any info related to a known issue related to workstation set up hardware.",
+    "text": "Style Guide Overview of the UI Components. Contact support @Nick Lyons. Text Sections Tables Lists Links Buttons Definition Lists Tabs Icon Box Text Sections Vertical Rhythm, or the spacing and proximity of headers and sub-headers in relation to paragraphs and lists is useful for visually communicating what is related. Notice the space above and below the headers, there is more above and less below to visually convey associated content. An equal distance above and below provides for a smooth read but doesn't show header to content relationship very well. Headers closer to their content shows a closeness, a relationship. It's a subtle effect. White space between headers shows distance or a distinct grouping of information. Use h2 headers for top level sections and use h3 for sub-sections or mini-chapters for a topic. These prinicples are coded for all elements on the page to provide a smoother reading experience. Hopefully... Vertical Rhythm Vertical Rhythm Vertical Rhythm Project Decomp What UI changes are happening, feed changes, database table validation. Ask the dev what will I be validating? The UI, the database tables, or a report somewhere? Testers and Dev's give a point estimate for suggested cards. Most story cards never exceed 5 points. If they do, the card is too big. You should find scenarios to move into another card. Don't estimate an 8 point card, decompose it into smaller cards. 3 Amigos RA creates story cards according to the SRD Business Rules. RA get's approval from BAO for the story card. Look at the RA Board for story card status. Bring laptop and ask the RA or Dev where in the UI or database tables the test can be validated. Write your notes in the RTC card, these will be helpful for the tester who picks up the card later. Tables Tables come into to types: Static or Data. Data tables allow you to use a YAML or CSV to populate the table from a file. Much easier for re-use and maintenance. Name Title Lists List of Links Tools Access & Permissions Starter Docs Useful Links/Documents Project Overview Testing Overview Cardwall How To's Contacts & Teams Inline Link in a Paragrah What UI changes are happening, feed changes, database table validation. Ask the dev what will I be validating? The UI, the database tables, or a report somewhere? Testers and Dev's give a point estimate for suggested cards. Quick Links Quick Links list for article sections. Put an id=\"#bookmark\" on an element and reference it from the quick links. Quick links go at the beginning and basically short cuts to the article sections. About More Info Product Model State Variations Details of Product Types Product Codes Serialization Checkbox List Oracle Database - Path to Download User Interface - Path to Download SQL Developer - Path to Download Buttons Used for navigation to other pages. Colors are for variety! Home Onboarding Projects Back to Home Data Definition Lists Hide and Show definitions are perfect for keeping a growing list of business and technical terms. Basic Policy Terms Deductible The amount the customer must pay before insurance kicks in. Coverage A general term for all the types of coverages against possible risks. The more coverage the higher the premium. A general term for all the types of coverages against possible risks. The more coverage the higher the premium. Primary Insured Usually the policy holder, it refers to who the insurance will protect. MVR Motor Vehicle Records contain traffic violations and related information about the vehicle or driver. Secondary Insured Additional individual to be insured under the same coverages as the Primary. Tabs Tabs are great for displaying data with images and text. Story Card Tech Card Defect Card Error Card They are linked to RTC. Developer works on the card, then moves ready to test for line tester to work on. They are sent the BAO for Sign-Off when testing is complete. Tech cards are worked by only the developer. They are not tested by the line. Defect cards are worked on by Line Developer, then sent back to tester who created it. In Vegas line, these are defects found outside of the team and assigned to our dev's to fix. Line Error cards are fixed by Line Dev, then retested by Line Tester who found them. In Vegas line, they are 'defects' found within line testing. Icon Box Good for hightlighting a tip, trick, gotcha or a don't do. Warning: Don't Do this! This will cause permanent damage to the system. Or will screw up the database/policy/data sheet. This will cause permanent damage to the system. Or will screw up the database/policy/data sheet. Save for later. You'll use this information often for user testing. Keep it handy or bookmarked for later review. Save for later. You'll use this information often for user testing. Keep it handy or bookmarked for later review. Good Idea! Try this to save time, or a nifty process to assist in what ever you are doing. System/Computer Configuration! Any info related to a known issue related to workstation set up hardware.",
     "tags": "",
-    "url": "/style-guide/"
+    "url": "/lyonsn1/git-wiki/style-guide/"
   },{
     "title": "Test Plans",
     "text": "Test Plans Testing Overview Info goes here.",
     "tags": "",
-    "url": "/testing/test-plans/"
+    "url": "/lyonsn1/git-wiki/testing/test-plans/"
   },{
     "title": "Testing Overview",
     "text": "Testing Overview What and How What we don't do",
     "tags": "",
-    "url": "/onboarding/testing-overview/"
+    "url": "/lyonsn1/git-wiki/onboarding/testing-overview/"
   },{
     "title": "Testing",
     "text": "Line Testing Testing Overview Test Plans Defects How To's",
     "tags": "",
-    "url": "/testing/home/"
+    "url": "/lyonsn1/git-wiki/testing/home/"
   },{
     "title": "onboarding - tools",
-    "text": "Tools Soap UI SQL Explorer",
+    "text": "Tools Oracle Database - Path to Download User Interface - Path to Download SQL Developer - Path to Download Access/Permissions UI - try making a payment SQL Server - ask Tech lead for permissions RTC - Ask IM for access RRC - Ask IM for access",
     "tags": "",
-    "url": "/onboarding/tools/"
+    "url": "/lyonsn1/git-wiki/onboarding/tools-access/"
   },{
     "title": "Transactions",
-    "text": "Transactions Name Code Transaction Code Payment PA 70 Pour-In PI 72 Fund Transfer XF 90 Asset Rebalancing ARB 94 Dollar-Cost Averaging DC 95 Automatic Exchange AT 96 End-Result Exchange ER 100 Partial Surrender WD 150 Loan LO 170 Loan Repayment LR 140 Surrender SR 190 Death DH 200 Insurance Charge MD 210 Lapse LP 220",
+    "text": "Transactions Name Code Transaction Code {% for transaction in site.data.transactions %} {{transaction.name}} {{transaction.code}} {{transaction.trxnumber}} {% endfor %}",
     "tags": "",
-    "url": "/transactions/home/"
+    "url": "/lyonsn1/git-wiki/transactions/home/"
   },{
     "title": "Useful Links",
     "text": "Useful Links",
     "tags": "",
-    "url": "/onboarding/useful-links/"
+    "url": "/lyonsn1/git-wiki/onboarding/useful-links/"
   }]};
