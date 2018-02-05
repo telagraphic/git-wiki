29c9c7dc5ad33e0fc16b4b3b41821872949689bd
lyonsn1 <lyonsn1@nationwide.com>
2018-02-05 13:41:18 -0500

    fixed menu search width glitch, added keyboard shortcuts

--- _site/js/layouts/keyboard-shortcuts.js
+++ _site/js/layouts/keyboard-shortcuts.js
@@ -1,44 +1,37 @@
-// page.addEventListener('keydown', function(event) {
-//   if (event.keyCode === 17 && event.keyCode === 109) {
-//     console.log('menu');
-//   }
-//
-//   if (event.keyCode === 17 && event.keyCode === 115) {
-//     console.log('search');
-//   }
-// });
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
 
-window.addEventListener("keydown", keysPressed, false);
-window.addEventListener("keyup", keysReleased, false);
+  if (event.ctrlKey && keyName === "m") {
+    hamburger.classList.toggle("is-active");
+    menu.classList.toggle('show-menu');
+    handle.classList.toggle('white-handle');
+    circle.classList.toggle('white-circle');
+    content_section.classList.toggle('toggle-content');
+    nav_icons.classList.toggle('toggle-icons');
+  } else if (event.ctrlKey && keyName === "x") {
+    search_icon.classList.toggle("open");
+    nav_icons.classList.toggle('toggle-icons');
+    nav.classList.toggle("nav-full-width");
+    search_form.classList.toggle('toggle-search-form');
+    nav.classList.toggle('toggle-nav-background');
+    search_input.focus();
+  } else if (keyName === "Escape") {
+    search_form.classList.remove('toggle-search-form');
+    nav.classList.remove('toggle-nav-background');
+    search_icon.classList.remove('open');
+    search_section.classList.remove('toggle-results');
+    nav_icons.classList.remove('toggle-icons');
+    content_section.classList.remove('toggle-content');
+    handle.classList.remove('white-handle');
+    circle.classList.remove('white-circle');
+    event.preventDefault();
+  }
 
-var keys = [];
 
-function keysPressed(e) {
-    console.log("key pressed..");
-    console.log(keys[e.keyCode]);
-    // store an entry for every key pressed
-    keys[e.keyCode] = true;
 
-    // Ctrl + f
-    if (keys[17] && keys[109]) {
-        // do something
-        console.log('menu');
+}, false);
 
-        // prevent default browser behavior
-        e.preventDefault();
-    }
-
-    // Ctrl + f
-    if (keys[17] && keys[115]) {
-        // do something
-        console.log('menu');
-
-        // prevent default browser behavior
-        e.preventDefault();
-    }
-}
-
-function keysReleased(e) {
-    // mark keys that were released
-    keys[e.keyCode] = false;
-}
+document.addEventListener('keyup', (event) => {
+  const keyName = event.key;
+  event.preventDefault();
+}, false);
